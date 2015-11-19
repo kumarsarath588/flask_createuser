6b62de30deed455feadff8657a2fa26a10d84b4f
kumarsarath588 <sarathkumar>
2015-11-20 00:52:52 +0530

    Added post variables and printing

--- app/user/user.py
+++ app/user/user.py
@@ -5,4 +5,7 @@ from flask import render_template,request
   if request.method == 'GET':
      return render.template('user/user_action.html')
   elif request.method == 'POST':
-     return 'Successfully submited'
+     username = request.data['username']
+     password = request.data['password']
+     shell = request.data['shell']
+     return 'Successfully submited' + username + password + shell
