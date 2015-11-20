068b6db77c44a400cec750f8e8c24a5037bfee4c
kumarsarath588 <kumar.sarath588@gmail.com>
2015-11-20 10:52:05 +0530

    User existance completed

--- user_create.py
+++ user_create.py
@@ -1,2 +1,17 @@
 import subprocess
 
+username = 'kumarsarath588'
+if not username.isalnum():
+  print("Username {} can only contain [a-z,A-z,1-9]".format(username))
+
+def userExistance(username):
+  try:
+    args = ['gettent', 'passwd', username]
+    cmd = subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    out,error = cmd.communicate()
+    return cmd.returncode
+    print(cmd.returncode)
+  except:
+    print("Error occured while executing '{}'".format(' '.join(args)))
+
+userExistance('root')
