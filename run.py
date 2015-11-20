945b353834a35424d5bfc4fcae7ade592d8c960d
Kumar, Sarath <sarathkumar@ge.com>
2015-11-20 23:37:36 +0530

    Initialized logging

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 from app import app
 import os
+import logging
+
+if not os.path.exists('log'):
+        os.makedirs('log')
+
+logging.basicConfig(filename='log/app.log', level=logging.INFO)
 
 if os.getuid() != 0:
-  print("This applications requires elevations")
+  log.error('This applications requires elevations')
+  print('This applications requires elevations')
 
 app.debug = True
 app.run(host='0.0.0.0', port=80)
