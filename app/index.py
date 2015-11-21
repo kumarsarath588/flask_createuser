275420a6f260a6ad899f07b00671e9a60d06da48
kumarsarath588 <kumar.sarath588@gmail.com>
2015-11-21 11:47:43 +0530

    Working perfectly

--- app/index.py
+++ app/index.py
@@ -3,6 +3,5 @@ from flask import render_template,url_for
 
 @app.route('/')
 def index():
-  print(url_for('user'))
   processes = url_for('user')
   return render_template('index.html',processes = processes)
