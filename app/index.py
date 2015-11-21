2fd54b66dc5faa64477ffad9c1d9770b9fd0b002
kumarsarath588 <kumar.sarath588@gmail.com>
2015-11-21 12:25:16 +0530

    Index page rendering from script & resolved modify/sudo

--- app/index.py
+++ app/index.py
@@ -3,5 +3,5 @@ from flask import render_template,url_for
 
 @app.route('/')
 def index():
-  processes = url_for('user')
+  processes = [ url_for('user') ]
   return render_template('index.html',processes = processes)
